8982c1664ccfd0885bb6c500265ff80e45b0681a
Benjamin Heath <benjaminheath238@gmail.com>
2022-06-19 20:22:02 +0100

    Changed items tab image

--- src/main/java/com/bjmh/scivity/gui/ScivityTabs.java
+++ src/main/java/com/bjmh/scivity/gui/ScivityTabs.java
@@ -1,6 +1,7 @@
 package com.bjmh.scivity.gui;
 
 import com.bjmh.scivity.block.ModBlocks;
+import com.bjmh.scivity.item.ModItems;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
@@ -22,7 +23,7 @@ public class ScivityTabs {
     public static final CreativeTabs ScivityItemsTab = new CreativeTabs("scivity_items_tab") {
         @SideOnly(Side.CLIENT)
         public ItemStack getTabIconItem() {
-            return new ItemStack(ModBlocks.STRIPES_PANEL_BLACK_YELLOW);
+            return new ItemStack(ModItems.CIRCUIT_ADVANCED);
         }
     };
 
