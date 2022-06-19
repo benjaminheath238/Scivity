5548843adc3dacfd796c6dd57007d86a6bebfa36
Benjamin Heath <benjaminheath238@gmail.com>
2022-06-19 18:47:52 +0100

    Fixed item models

--- src/main/java/com/bjmh/scivity/item/ScivityItem.java
+++ src/main/java/com/bjmh/scivity/item/ScivityItem.java
@@ -3,7 +3,11 @@ package com.bjmh.scivity.item;
 import com.bjmh.scivity.Scivity;
 import com.bjmh.scivity.gui.ScivityTabs;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ScivityItem extends Item {
     public ScivityItem(String name) {
@@ -11,5 +15,10 @@ public class ScivityItem extends Item {
         setRegistryName(Scivity.MODID + ":" + name);
         setCreativeTab(ScivityTabs.ScivityItemsTab);
         ModItems.ITEMS.add(this);
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 }
--- src/main/java/com/bjmh/scivity/proxy/ClientProxy.java
+++ src/main/java/com/bjmh/scivity/proxy/ClientProxy.java
@@ -2,8 +2,11 @@ package com.bjmh.scivity.proxy;
 
 import com.bjmh.scivity.block.ModBlocks;
 import com.bjmh.scivity.block.ScivityBlock;
+import com.bjmh.scivity.item.ModItems;
+import com.bjmh.scivity.item.ScivityItem;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,6 +18,10 @@ public class ClientProxy extends CommonProxy {
     public static void registerModels(ModelRegistryEvent event) {
         for (Block block : ModBlocks.BLOCKS) {
             ((ScivityBlock) block).initModel();
+        }
+
+        for (Item item : ModItems.ITEMS) {
+            ((ScivityItem) item).initModel();
         }
     }
 }
