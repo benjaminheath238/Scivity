b13d08d1f71804c3ce97b8e9a79bad1c739f88b5
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-04 08:13:48 +0100

    Started to add config

---
+++ src/main/java/com/bjmh/scivity/config/ScivityConfig.java
@@ -0,0 +1,10 @@
+package com.bjmh.scivity.config;
+
+import com.bjmh.scivity.Scivity;
+
+import net.minecraftforge.common.config.Config;
+
+@Config(modid = Scivity.MODID)
+public class ScivityConfig {
+    // public static String ad = "a";
+}
